6ab352ce97d8a5bd1d1b03eb068146dcbe54d5f0
Alex <alex.holder@bigfootds.com>
2025-02-17 20:55:07 +1100

    feat: pokemon team logic in place

--- src/components/PokemonTeamBuilder.jsx
+++ src/components/PokemonTeamBuilder.jsx
@@ -3,6 +3,7 @@
 // requestedName = string 
 
 import React from "react";
+import { PokemonDisplay } from "./PokemonDisplay";
 
 export class PokemonTeamBuilder extends React.Component {
 	constructor(){
@@ -12,11 +13,32 @@ export class PokemonTeamBuilder extends React.Component {
 			teamSize: 6,
 			requestName: ""
 		}
+
+		this.formSubmitHandler = this.formSubmitHandler.bind(this);
 	}
 
 	formSubmitHandler (event) {
 		event.preventDefault();
 		console.log("Event stopped! Form submission being handled now.");
+
+		// Make an array of fetch requests based on this.state.teamSize
+		const requestArray = [];
+		for (let index = 0; index < this.state.teamSize; index++) {
+			requestArray.push(this.getRandomPokemonData());
+		}
+
+		Promise.all(requestArray).then((totalData) => {
+			this.setState({
+				teamData: totalData
+			});
+		});
+	}
+
+	async getRandomPokemonData() {
+		let targetPokemonId = Math.floor(Math.random() * 1025);
+		let response = await fetch("http://pokeapi.co/api/v2/pokemon/" + targetPokemonId);
+		return response.json();
+		
 	}
 
 	render () {
@@ -33,11 +55,11 @@ export class PokemonTeamBuilder extends React.Component {
 						min={1}
 						max={6}
 						onChange={(event) => {
-							if (event.target.value > 6) {
-								event.target.value = 6;
-							} else if (event.target.value < 1) {
-								event.target.value = 1;
-							}
+							// if (event.target.value > 6) {
+							// 	event.target.value = 6;
+							// } else if (event.target.value < 1) {
+							// 	event.target.value = 1;
+							// }
 							this.setState({
 								teamSize: Number.parseInt(event.target.value)
 							});
@@ -45,6 +67,16 @@ export class PokemonTeamBuilder extends React.Component {
 					/>
 					<button type="submit">Submit</button>
 				</form>
+
+				{
+					this.state.teamData && this.state.teamData.map((individualPokemon, index) => {
+						return <PokemonDisplay 
+							key={individualPokemon.name + index }
+							sprite={individualPokemon.sprites.front_default} 
+							name={individualPokemon.name} 
+						/>
+					})
+				}
 			</section>
 		)
 	}
